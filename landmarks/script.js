d0c7124983f586c5ec99566c9b7f6f5804d760f9
ktsiam <ktsiampouris@gmail.com>
2017-10-21 17:47:06 -0400

    add icons

--- landmarks/script.js
+++ landmarks/script.js
@@ -3,7 +3,7 @@ var myLng = -71.1;
 var request = new XMLHttpRequest();
 var me = new google.maps.LatLng(myLat, myLng);
 var myOptions = {
-	zoom: 13,
+	zoom: 14,
 	center: me,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
@@ -12,9 +12,9 @@ var marker;
 var infowindow = new google.maps.InfoWindow();
 
 var icon = {
-    url: "me.png",
-    scaledSize: new google.maps.Size(50, 50), // scaled size
-    origin: new google.maps.Point(0,0), // origin
+    url: "landmark.png",
+    scaledSize: new google.maps.Size(50, 50),
+    origin: new google.maps.Point(0,0),
     anchor: new google.maps.Point(0, 0) // anchor
 };
 
@@ -46,12 +46,12 @@ function renderMap()
 		position: me,
 		title: "That's me!",
         info: "hey",
-        //icon: icon,
+        icon: "me.png"
 	});
 	marker.setMap(map);		   
 
     google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent("Hello there Traveller!");
+		infowindow.setContent("It's me!");
 		infowindow.open(map, marker);
         });
 
@@ -67,7 +67,7 @@ function makeRequest()
             data = JSON.parse(this.responseText);
             getSmallestDistance();
             setUsers();
-            //landmarks();
+            setLandmarks();
         }
     }
     request.send("login=S3hcFNS0&lat=" + myLat + "&lng=" + myLng);
@@ -91,13 +91,14 @@ function getSmallestDistance()
                 }
         }
     
-    marker.info = "Closest Landmark: " + data.landmarks[idx].properties.Location_Name;
-    marker.info += "<br>" + "Distance from closest landmark : " + min + "miles";
+    marker.info = "Closest Landmark: " + data.landmarks[idx].properties.Location_Name
+        + "<br>" + "Distance from closest landmark : " + min + "miles";
 
-    way = new google.maps.Polyline({
-            path: [me, topLoc]
+    line = new google.maps.Polyline({
+            path: [me, topLoc],
+            strokeColor: '#03f2e0'
         });
-    way.setMap(map);
+    line.setMap(map);
 }
 
 function toRad(a){
@@ -105,7 +106,7 @@ function toRad(a){
 }
 function distance(a, b)
 {
-    var R = 6371e3; // metres
+    var R = 6371e3;
     var f1 = toRad(a.lat());
     var f2 = toRad(b.lat());
     var Df = toRad((b.lat()-a.lat()));
@@ -115,27 +116,54 @@ function distance(a, b)
         Math.cos(f2) * Math.cos(f2) *
         Math.sin(Dl/2) * Math.sin(Dl/2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    return R * c;
+    d = R * c;
+    return (d / 1609.34).toFixed(3); //converting to miles
 }
 
-function setUsers() {    
+function setUsers() 
+{
     for (i = 0; i < data.people.length; ++i) {
         latit = data.people[i].lat;
         longit = data.people[i].lng;
         var loc = new google.maps.LatLng(latit, longit);
         var dist = distance(loc, me);
-
+        
         var user = new google.maps.Marker({
-                position: location,
+                position: loc,
                 title: "Others",
                 info: "Login: " + data.people[i].login
-                + "Distance from me: " + dist + " miles"
+                + "</br>Distance from me: " + dist + " miles"
             });
-        user.setMap(map);
-
-        google.maps.event.addListener(user, 'click', function() {
-                infoWindow.setContent(this.info);
-                infoWindow.open(map, this);
-            });
+        user.setMap(map); 
+        displayInfo(user);
     }
 }
+
+function setLandmarks()
+{
+    for (i = 0; i < data.landmarks.length; ++i) {
+        longitude = data.landmarks[i].geometry.coordinates[0];
+        latitude = data.landmarks[i].geometry.coordinates[1];
+        var location = new google.maps.LatLng(latitude, longitude);
+        
+        var landmarker = new google.maps.Marker({
+                position: location,
+                icon: icon,
+                title: "Landmark",
+                info: data.landmarks[i].properties.Details
+            });
+        landmarker.setMap(map);
+        displayInfo(landmarker);
+    }
+}
+
+
+var infoWindow = new google.maps.InfoWindow();
+function displayInfo(marker)
+{
+    google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.setContent(this.info);
+            infoWindow.open(map, this);
+        });
+}
+
