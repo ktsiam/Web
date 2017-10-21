3530eba060193efc48e4961677fb65378d0f2c59
ktsiam <ktsiampouris@gmail.com>
2017-10-21 16:56:43 -0400

    add route

--- landmarks/script.js
+++ landmarks/script.js
@@ -45,7 +45,8 @@ function renderMap()
 	marker = new google.maps.Marker({
 		position: me,
 		title: "That's me!",
-        icon: icon,
+        info: "hey",
+        //icon: icon,
 	});
 	marker.setMap(map);		   
 
@@ -64,8 +65,9 @@ function makeRequest()
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.responseText);
-            console.log(data);
             getSmallestDistance();
+            setUsers();
+            //landmarks();
         }
     }
     request.send("login=S3hcFNS0&lat=" + myLat + "&lng=" + myLng);
@@ -73,5 +75,67 @@ function makeRequest()
 
 function getSmallestDistance()
 {    
-    google.maps.geometry.spherical.computeDistanceBetween(latLngA, latLngB);
+    var min = Infinity;
+    var lat, lng, loc, dist, idx, topLoc;
+    for (i = 0; i < data.landmarks.length; ++i)
+        {
+            lng = data.landmarks[i].geometry.coordinates[0];
+            lat = data.landmarks[i].geometry.coordinates[1];
+            loc = new google.maps.LatLng(lat, lng);            
+            dist = distance(me, loc);
+            if (dist < min)
+                {
+                    min = dist;
+                    idx = i;
+                    topLoc = loc;
+                }
+        }
+    
+    marker.info = "Closest Landmark: " + data.landmarks[idx].properties.Location_Name;
+    marker.info += "<br>" + "Distance from closest landmark : " + min + "miles";
+
+    way = new google.maps.Polyline({
+            path: [me, topLoc]
+        });
+    way.setMap(map);
+}
+
+function toRad(a){
+    return a / 180 * Math.PI
+}
+function distance(a, b)
+{
+    var R = 6371e3; // metres
+    var f1 = toRad(a.lat());
+    var f2 = toRad(b.lat());
+    var Df = toRad((b.lat()-a.lat()));
+    var Dl = toRad((b.lng()-a.lng()));
+
+    var a = Math.sin(Df/2) * Math.sin(Df/2) +
+        Math.cos(f2) * Math.cos(f2) *
+        Math.sin(Dl/2) * Math.sin(Dl/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return R * c;
+}
+
+function setUsers() {    
+    for (i = 0; i < data.people.length; ++i) {
+        latit = data.people[i].lat;
+        longit = data.people[i].lng;
+        var loc = new google.maps.LatLng(latit, longit);
+        var dist = distance(loc, me);
+
+        var user = new google.maps.Marker({
+                position: location,
+                title: "Others",
+                info: "Login: " + data.people[i].login
+                + "Distance from me: " + dist + " miles"
+            });
+        user.setMap(map);
+
+        google.maps.event.addListener(user, 'click', function() {
+                infoWindow.setContent(this.info);
+                infoWindow.open(map, this);
+            });
+    }
 }
